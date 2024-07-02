6e01af1779761b93b87a0e989eece34e6bd48e7b
Hsegunn <segun9909@naver.com>
2024-07-02 18:41:04 +0900

    초기세팅

---
+++ weatherprj/src/main/java/com/teamprj/weatherprj/WeatherprjApplication.java
@@ -0,0 +1,13 @@
+package com.teamprj.weatherprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherprjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherprjApplication.class, args);
+	}
+
+}
---
+++ weatherprj/src/test/java/com/teamprj/weatherprj/WeatherprjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamprj.weatherprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherprjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
