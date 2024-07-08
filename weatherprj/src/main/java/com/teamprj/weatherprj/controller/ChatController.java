c935a971145249ee434e7f8e50ec84b340dd3801
ch9729 <karlll@naver.com>
2024-07-08 21:46:41 +0900

    openAI api 수정중

--- weatherprj/src/main/java/com/teamprj/weatherprj/controller/ChatController.java
+++ weatherprj/src/main/java/com/teamprj/weatherprj/controller/ChatController.java
@@ -2,11 +2,21 @@ package com.teamprj.weatherprj.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Arrays;
+import io.github.flashvayne.chatgpt.dto.chat.MultiChatMessage;
+import io.github.flashvayne.chatgpt.service.ChatgptService;
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class ChatController {
+    
+    private final ChatgptService chatgptService;
+
     @GetMapping("/chat")
     public String getMethodName() {
+        String responseMessage = chatgptService.multiChat(Arrays.asList(new MultiChatMessage("user", "반가워 너는 누구니?")));
+        System.out.println(responseMessage);
         return "chat";
     }
 }
